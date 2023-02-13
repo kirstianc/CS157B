5b5f9082029b53805fbc9869a913a48d4d624cc2
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-02-13 14:24:58 -0800

    connection issues
    fixed connection, must fix empty table sql code

--- HW1/RecordTester.java
+++ HW1/RecordTester.java
@@ -2,79 +2,131 @@ import java.sql.*;
 import java.util.Random;
 
 public class RecordTester{
-    public static void main(String[] args) {
-      
-      RecordTester rt = new RecordTester();
-      String dbms = args[0];
-      String db = args[1];
-      String table_name = args[2];
-      String type = args[3];
-      int num_rows = Integer.parseInt(args[4]);
-      int num_columns = Integer.parseInt(args[5]);
-      String index = "";
+  private static final String MYSQL_DB_URL = "";
+  private static final String SQLITE_DB_URL = "jdbc:sqlite:./";
+  private static final String USERNAME = "root";
+  private static final String PASSWORD = "1234";
+
+  public String dbms;
+  public String db;
+  public String table_name;
+  public String type;
+  public int num_rows;
+  public int num_columns;
+    
+  public static void main(String[] args) {
+    RecordTester rt = new RecordTester(args);
+  }
+
+  public RecordTester(String[] args){
+      dbms = args[0];
+      db = args[1];
+      table_name = args[2];
+      type = args[3];
+      num_rows = Integer.parseInt(args[4]);
+      num_columns = Integer.parseInt(args[5]);
       StringBuilder addRowSB = new StringBuilder();
 
       try{
+        Connection connection = null;
+
+        if(dbms.equals("sqlite")){
+          connection = connectToDB(0);
+        }else if(dbms.equals("mysql")){
+          connection = connectToDB(1);
+        }else{
+          System.out.println("Must be using mysql or sqlite");
+        }
+
         //load JDBC driver
         Class.forName("org.sqlite.JDBC");
-        
+        System.out.println("loaded JDBC driver");
+
         //db connection
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:dbms.db");
+        //Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/Mom/Desktop/cs166/CS157B/HW1/test.sql");
+        //System.out.println("db connection");
         
         //sql statement used for all sql commands
         Statement statement = connection.createStatement();
+        System.out.println("created statement");
         
         //drop table if already exists
-        String dropTable = "DROP TABLE IF EXISTS " + table_name;
+        String dropTable = "DROP TABLE IF EXISTS " + table_name + ";";
         statement.executeUpdate(dropTable);
-
-        //create table
-        String createTable = "CREATE TABLE " + table_name;
+        System.out.println("dropped table if exist");
+        
+        //create table CANT CREATE EMPTY TABLE W NO COLS, MUST FIX
+        System.out.println("attempting creating table");
+        String createTable = "CREATE TABLE " + table_name + ";";
         statement.executeUpdate(createTable);
-
+        System.out.println("created table");
+        
         //add cols, named by i
         String addCol = ""; 
         for(int i=0; i<num_columns; i++){
-          addCol = "ALTER TABLE "+ i + " " + type;
+          addCol = "ALTER TABLE "+ i + " " + type + ";";
           statement.executeUpdate(addCol);
         }
-
+        System.out.println("added cols");
+        
         //if index then set to have index related to first col (0)
         if(args[6].equals("TRUE")){
-          String addIndex = "ALTER TABLE " + table_name +" ADD UNIQUE INDEX index_name 0";
+          String addIndex = "ALTER TABLE " + table_name +" ADD UNIQUE INDEX index_name 0" + ";";
           statement.executeUpdate(addIndex);
         }
-
+        System.out.println("added index");
+        
         //add rows, randomly generated vals using helper function
         String addRow = ""; 
         String temp = "";
         for(int i=0; i<num_rows; i++){
-          addRow = "INSERT INTO "+ table_name + " " + i;
+          addRow = "INSERT INTO "+ table_name + " " + i + ";";
           addRowSB.append(addRow);
           for(int j=0; j<num_columns; j++){
-            temp = " " + rt.randomRun(type);
+            temp = " " + randomRun(type);
             addRowSB.append(temp);
           }
           addRow=addRowSB.toString();
           statement.executeUpdate(addRow);
         }
-
+        System.out.println("added rows");
         
-
         } catch(Exception e){
-
-        }        
-    }
-
-    private String randomRun(String type){
-      StringBuilder sb = new StringBuilder();
-      Random random = new Random();
-    
-      if(type.charAt(0)=='T'){
-        int len = random.nextInt(100) + 1;
-        for (int i = 0; i < len; i++) {
-          sb.append(random.nextInt(2));
+          System.out.println("Exception " + e);
         }
+  }
+
+  private Connection connectToDB(int dbType){
+    Connection connection = null;
+    if(dbType==0){
+      try{
+        connection = DriverManager.getConnection(SQLITE_DB_URL + db);
+        System.out.println("Connected to SQLite DB");
+      }catch(SQLException e){
+        System.out.println(e);
+      }
+    }else if(dbType==1){
+      try{
+        connection = DriverManager.getConnection(MYSQL_DB_URL, USERNAME, PASSWORD);
+        System.out.println("Connected to SQLite DB");
+      }catch(SQLException e){
+        System.out.println(e);
+      }
+    }else{
+      System.out.println("Error connection to DB, must be MySQL or SQLite");
+    }
+    return connection;
+  }
+
+
+  private String randomRun(String type){
+    StringBuilder sb = new StringBuilder();
+    Random random = new Random();
+    if(type.charAt(0)=='T'){
+      int len = random.nextInt(100) + 1;
+      for (int i = 0; i < len; i++) {
+        sb.append(random.nextInt(2));
+      }
       }else{
         for(int i=0; i<99; i++){
           sb.append(random.nextInt(2));
